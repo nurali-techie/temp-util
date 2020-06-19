20aea33fd0ac0c083c3ab4a20f09ff8c9e6c08d3
nurali-techie <nurali.techie@gmail.com>
2020-06-19 22:45:22 +0530

    fix: remote extra space in input

--- util_test.go
+++ util_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestUserName(t *testing.T) {
 	want := "nurali-virani"
-	if got := UserName("Nurali", "Virani"); got != want {
+	if got := UserName(" Nurali ", " Virani "); got != want {
 		t.Errorf("UserName() = %q, want = %q", got, want)
 	}
 }
