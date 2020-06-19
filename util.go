1f6901baf155a4246d1955117df95151cae0e210
nurali-techie <nurali.techie@gmail.com>
2020-06-19 23:18:25 +0530

    GenEmail func added

--- util.go
+++ util.go
@@ -7,17 +7,25 @@ import (
 	"strings"
 )
 
-// UserName joins given firstName and lastName and return string in UserName format
-func UserName(firstName, lastName string) string {
+// UserName joins given firstName, lastName, middleName and return string in UserName format
+func UserName(firstName, lastName, middleName string) string {
 	firstName = strings.TrimSpace(firstName)
 	lastName = strings.TrimSpace(lastName)
+	middleName = strings.TrimSpace(middleName)
 
 	firstName = strings.ToLower(firstName)
 	lastName = strings.ToLower(lastName)
+	middleName = strings.ToLower(middleName)
 
-	return fmt.Sprintf("%s-%s", firstName, lastName)
+	initial := middleName[0]
+	return fmt.Sprintf("%s-%s-%c", firstName, lastName, initial)
 }
 
 func GeneratePassword() string {
 	return strconv.Itoa(rand.Intn(1000))
+}
+
+func GenerateEmail(userName string) string {
+	userName = strings.ReplaceAll(userName, "-", ".")
+	return fmt.Sprintf("%s@mycompany.com", userName)
 }
--- util_test.go
+++ util_test.go
@@ -3,8 +3,8 @@ package util
 import "testing"
 
 func TestUserName(t *testing.T) {
-	want := "nurali-virani"
-	if got := UserName(" Nurali ", " Virani "); got != want {
+	want := "nurali-virani-b"
+	if got := UserName(" Nurali ", " Virani ", "Barkatali"); got != want {
 		t.Errorf("UserName() = %q, want = %q", got, want)
 	}
 }
@@ -12,5 +12,12 @@ func TestUserName(t *testing.T) {
 func TestGeneratePassword(t *testing.T) {
 	if got := GeneratePassword(); len(got) == 0 {
 		t.Errorf("Invalid password generated")
+	}
+}
+
+func TestGenerateEmail(t *testing.T) {
+	want := "nurali.virani.b@mycompany.com"
+	if got := GenerateEmail("nurali-virani-b"); got != want {
+		t.Errorf("GenerateEmail() = %q, want = %q", got, want)
 	}
 }
