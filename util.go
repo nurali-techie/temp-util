0d913c773b7239eef0ffd7bd5f84c8dba1d805bb
nurali-techie <nurali.techie@gmail.com>
2020-06-19 22:28:28 +0530

    doc added

--- util.go
+++ util.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 )
 
+// UserName joins given firstName and lastName and return string in UserName format
 func UserName(firstName, lastName string) string {
 	name := fmt.Sprintf("%s-%s", strings.ToLower(firstName), strings.ToLower(lastName))
 	return name
